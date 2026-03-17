f71ff222944edca83156a6cda8569c3f0e7cf6a9
Kyriell <kirya.shmidt.0600@mail.ru>
2026-03-18 00:14:20 +0300

    feat:finalize notification-service setup

---
+++ cmd/notification-service/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	notificationadapter "github.com/Chaice1/Order-Service/internal/notification/adapter"
+	notificationservice "github.com/Chaice1/Order-Service/internal/notification/service"
+	"github.com/joho/godotenv"
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	godotenv.Load()
+
+	TgBot := notificationservice.NewTelegramBot(os.Getenv("CHAT_ID"), os.Getenv("BOT_TOKEN"))
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{os.Getenv("KAFKA_ADDR")},
+		Topic:   "created_order",
+		GroupID: "notification_service",
+	})
+
+	consumer := notificationadapter.NewConsumer(reader, TgBot)
+
+	consumer.ConsumeMessage(context.Background())
+
+}
